32aaf971f5dfced5f69e2c699533ec5cd7694580
Nikhil Singh <92269431+Singhsansar@users.noreply.github.com>
2022-06-11 05:50:55 +0530

    Delete amritianmess directory

--- amritianmess/android/app/src/main/kotlin/com/example/amritianmess/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.amritianmess
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- amritianmess/lib/constants.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-const kprimarycolor = Color.fromARGB(255, 92, 19, 159);
-const kprimaryLightcolor = Color.fromARGB(255, 235, 190, 227);
