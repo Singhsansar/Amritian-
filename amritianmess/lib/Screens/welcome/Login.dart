4260753fc5656207233f9ae7877279c67fba2520
unknown <singhsansar433@gmail.com>
2022-06-11 15:50:10 +0530

    Done with Register page

--- amritianmess/lib/Screens/welcome/Login.dart
+++ amritianmess/lib/Screens/welcome/Login.dart
@@ -18,112 +18,118 @@ class Login extends StatelessWidget
      Size size = MediaQuery.of(context).size;
     //create a form over here  for the sign up 
     return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image:AssetImage(
-          "assets/icons/Amrita.jpg",
-          
-        ),
-        fit: BoxFit.cover,
-        ),
-      ),
-        child:  Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: [
-              Container(
-                padding: EdgeInsets.only(left:size.width*0.2,top: size.height*0.1),
-                  child: const Text("Welcome to Amrita",
-                  style:TextStyle(
-                  fontSize: 33,
-                  color: Color.fromARGB(255, 32, 42, 227),
-                  fontWeight:FontWeight.bold,
-
-                ),
-                ),
-            ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image:AssetImage(
+            "assets/icons/Amrita.jpg",
             
-               SingleChildScrollView( //for scrolling pages 
-                 child: Container(
-                  //margin: const EdgeInsets.symmetric(vertical: 8),
-                  width: size.width * 0.9,
-                  padding: EdgeInsets.only(top:size.height*0.4,left:size.width*0.2 ),
-                  child: Column(
-                    children:  [
-                      TextField(
-                        decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: "Email",
-                          border:OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),),
-                          
-                        ),
-               
-                      ),
-                      const SizedBox(
-                        height:10,
-                      ),
-                      TextField(
-                        obscureText: true,
-                        maxLength: 10,
-                            decoration: InputDecoration(
+          ),
+          fit: BoxFit.cover,
+          ),
+        ),
+          child:  Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(98, 74, 9, 9),
+            title: const Text("Log in"),
+          ),
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+                Container(
+                  padding: EdgeInsets.only(left:size.width*0.2,top: size.height*0.1),
+                    child: const Text("Welcome to Amrita",
+                    style:TextStyle(
+                    fontSize: 33,
+                    color: Color.fromARGB(255, 32, 42, 227),
+                    fontWeight:FontWeight.bold,
+    
+                  ),
+                  ),
+              ),
+              
+                 SingleChildScrollView( //for scrolling pages 
+                   child: Container(
+                    //margin: const EdgeInsets.symmetric(vertical: 8),
+                    width: size.width * 0.9,
+                    padding: EdgeInsets.only(top:size.height*0.4,left:size.width*0.2 ),
+                    child: Column(
+                      children:  [
+                        TextField(
+                          decoration: InputDecoration(
                             fillColor: Colors.grey.shade100,
                             filled: true,
-                            hintText: "Password",
+                            hintText: "Email",
                             border:OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),),
-                      ),
-                      ),
-                    RoundedBottom(
-                    text: "Login",
-                    textcolour: Colors.black,
-                     press: (){
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                          builder: (context){
-                          return const Signup();
-                
-                    },),);
-               },),
-               TextBottom(
-                text: "----------------  Forgotten Password ----------------", 
-                fontsiz: 15,
-                 press: (){
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                          builder: (context){
-                          return const Signup();
-                
-               },),);
-               },
+                              borderRadius: BorderRadius.circular(10),),
+                            
+                          ),
                  
-                 ),
-                TextBottom(
-                text: "--Sign up--", 
-                fontsiz: 20,
-                press: (){
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                          builder: (context){
-                          return const Signup();
-                
-                    },),);
-               },
-                 ),
-                    ],               
-                  ),
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                          obscureText: true,
+                          maxLength: 10,
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Password",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                      RoundedBottom(
+                      text: "Login",
+                      textcolour: Colors.black,
+                       press: (){
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                            builder: (context){
+                            return const Signup();
+                  
+                      },),);
+                 },),
+                 TextBottom(
+                  text: "Forgotten Password? Click here", 
+                  fontsiz: 15,
+                   press: (){
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                            builder: (context){
+                            return const Signup();
+                  
+                 },),);
+                 },
                    
+                   ),
+                  TextBottom(
+                  text: "Register", 
+                  fontsiz: 15,
+                  textcolour: Color.fromARGB(255, 5, 108, 193),
+                  press: (){
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                            builder: (context){
+                            return const Signup();
+                  
+                      },),);
+                 },
+                   ),
+                      ],               
+                    ),
+                     
+                   ),
                  ),
-               ),
-           
-          ],
-        ), //Stack and column will be same 
+             
+            ],
+          ), //Stack and column will be same 
+    
+        ),
+      );
 
-      ),
-    );
   }
 }
--- amritianmess/lib/Screens/welcome/Signup.dart
+++ amritianmess/lib/Screens/welcome/Signup.dart
@@ -1,4 +1,4 @@
-import 'package:amritamess/Components/Background.dart';
+import 'package:amritamess/Components/RoundedBottom.dart';
 import "package:flutter/material.dart";
 
 
@@ -13,59 +13,147 @@ class Signup extends StatelessWidget
   {
     Size size = MediaQuery.of(context).size;
     //create a form over here  for the sign up 
-    return  Background(
-      image: "assets/icons/AmritaVishwa.jpg",
-         child: Column(
-         children: [
-          Container(
-                  padding: EdgeInsets.only(left:size.width*0.1,top: size.height*0.1),
-                  child: const Text("Welcome to Amrita",
-                  style:TextStyle(
-                  fontSize: 33,
-                  color: Color.fromARGB(255, 32, 42, 227),
-                  fontWeight:FontWeight.bold,
-           ),),
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image:AssetImage(
+            "assets/icons/Amrita.jpg",
+            
+          ),
+          fit: BoxFit.cover,
+          ),
+        ),
+        child:  Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(98, 74, 9, 9),
+            title: const Text("Register"),
           ),
           
-         SingleChildScrollView( //for scrolling pages 
-                 child: Container(
-                  //margin: const EdgeInsets.symmetric(vertical: 8),
-                  width: size.width * 0.9,
-                  padding: EdgeInsets.only(top:size.height*0.4,left:size.width*0.2 ),
-                  child: Column(
-                    children:  [
-                      TextField(
-                        decoration: InputDecoration(
-                          fillColor: Colors.grey.shade100,
-                          filled: true,
-                          hintText: "Email",
-                          border:OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),),
-                          
-                        ),
-               
-                      ),
-                      const SizedBox(
-                        height:10,
-                      ),
-                      TextField(
-                        obscureText: true,
-                        maxLength: 10,
-                            decoration: InputDecoration(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+                Container(
+                  padding: EdgeInsets.only(left:size.width*0.2,top: size.height*0.1),
+                    child: const Text("Welcome to Amrita",
+                    style:TextStyle(
+                    fontSize: 33,
+                    color: Color.fromARGB(255, 83, 23, 23),
+                    fontWeight:FontWeight.bold,
+    
+                  ),
+                  ),
+              ),
+              
+                 SingleChildScrollView( //for scrolling pages 
+                   child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    width: size.width * 0.9,
+                   padding: EdgeInsets.only(top:size.height*0.2,left:size.width*0.2 ),
+                    child: Column(
+                      children:  [
+                        TextField(
+                          decoration: InputDecoration(
                             fillColor: Colors.grey.shade100,
                             filled: true,
-                            hintText: "Password",
+                            hintText: "First Name",
                             border:OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),),
-                      ),
-                      ),],
-         
+                              borderRadius: BorderRadius.circular(10),),
+                            
+                          ),
+                 
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                          //obscureText: true,
+                          //maxLength: 10,
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Last Name",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Email id",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                          //obscureText: true,
+                          //maxLength: 10,
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "+91  ",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                          //obscureText: true,
+                          //maxLength: 10,
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Branch",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                        const SizedBox(
+                          height:10,
+                        ),
+                        TextField(
+                          //obscureText: true,
+                          //maxLength: 10,
+                              decoration: InputDecoration(
+                              fillColor: Colors.grey.shade100,
+                              filled: true,
+                              hintText: "Hostel Name",
+                              border:OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),),
+                        ),
+                        ),
+                        
+                      RoundedBottom(
+                      text: "Register",
+                      textcolour: Colors.black,
+                       press: (){
+                          Navigator.push(
+                            context, 
+                            MaterialPageRoute(
+                            builder: (context){
+                            return const Signup();
+                  
+                      },),);
+                 },),
+                      ],               
+                    ),
+                     
+                   ),
+                 ),
+             
+            ],
+          ), //Stack and column will be same 
+    
+        ),
+      );
 
-         ),
-
-    )
-         )
-         
-   ] ));
   }
-}
+} 
